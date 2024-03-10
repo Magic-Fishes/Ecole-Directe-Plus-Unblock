ba3177aa0c175df0b60b69692225c192d886c2b9
Truite Séchée <truite.berthail@gmail.com>
2024-03-10 05:14:42 +0100

    Setup pop-up, setup promoteEDP on www.ecoledirecte.com website

--- promoteEDP.js
+++ promoteEDP.js
@@ -1,1 +1,9 @@
-console.log("EDP > ED")
+// embrigadement en cours
+console.log("EDP > ED");
+document.body.innerHTML = "<p>Et pourquoi pas utiliser <a href='https://ecole-directe.plus/login'>Ecole Directe Plus</a> ?<p>";
+document.body.style.backgroundColor = "#181829";
+document.body.style.color = "white";
+document.body.style.position = "absolute";
+document.body.style.top = "50%";
+document.body.style.left = "50%";
+document.body.style.transform = "translate(-50%, -50%)";
