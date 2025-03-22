648332a0e7ca3bb31f855219a3eef3836b6b8564
Mikkel ALMONTE--RINGAUD <contact@vexcited.com>
2025-03-22 03:15:20 +0100

    refactor: move firefox to mv3 and merge both service workers

--- service-worker.js
+++ service-worker.js
@@ -1,35 +1,26 @@
-console.log("Service Worker => ON")
 chrome.declarativeNetRequest.updateEnabledRulesets(
     { enableRulesetIds: ["change_origin"] }
 )
 
-try {
-    chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
-        if (changeInfo.status === "complete") {
-            chrome.scripting.executeScript({
-                target: { tabId: tab.id },
-                files: ["DOMInjectionBridge.js"]
-            });
-        }
-    });
-} catch(e) {
-    console.log("e", e)
-}
+chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+    if (changeInfo.status === "complete") {
+        chrome.scripting.executeScript({
+            target: { tabId: tab.id },
+            files: ["DOMInjectionBridge.js"]
+        });
+    }
+});
 
-try {
-    chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-        if (message.type === "GET_EXTENSION_INFO") {
-            const extensionInfo = {
-                name: chrome.runtime.getManifest().name,
-                version: chrome.runtime.getManifest().version
-            };
-            sendResponse(extensionInfo);
-        }
-        return true;
-    });
-} catch(e) {
-    console.log("e", e)
-}
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+    if (message.type === "GET_EXTENSION_INFO") {
+        const extensionInfo = {
+            name: chrome.runtime.getManifest().name,
+            version: chrome.runtime.getManifest().version
+        };
+        sendResponse(extensionInfo);
+    }
+    return true;
+});
 
 let gtk = null;
 
@@ -98,8 +89,20 @@ function interceptCookieGTK(details) {
   return { responseHeaders: headers };
 }
 
+function getBrowser() {
+  if (typeof chrome !== "undefined") {
+    if (typeof browser !== "undefined") {
+      return "Firefox";
+    } else {
+      return "Chrome";
+    }
+  } else {
+    return "Edge";
+  }
+}
+
 chrome.webRequest.onHeadersReceived.addListener(
   interceptCookieGTK,
   { urls: ["*://api.ecoledirecte.com/*"] },
-  ["responseHeaders", "extraHeaders"]
+  getBrowser() === "Firefox" ? ["responseHeaders"] : ["responseHeaders", "extraHeaders"]
 );
