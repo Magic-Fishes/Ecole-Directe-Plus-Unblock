55adc78ccb4c91f9ef5cb121347a05f82d44c8f6
Mikkel ALMONTE--RINGAUD <contact@vexcited.com>
2025-03-22 02:35:05 +0100

    fix: cookies with chrome

--- index.chromium.js
+++ index.chromium.js
@@ -30,3 +30,76 @@ try {
 } catch(e) {
     console.log("e", e)
 }
+
+let gtk = null;
+
+async function updateGtkRules() {
+  const removeRuleIds = await chrome.declarativeNetRequest.getDynamicRules()
+    .then(rules => rules.map(rule => rule.id));
+
+  const rules = [];
+
+  if (gtk) {
+    rules.push({
+      id: 10,
+      priority: 1,
+      condition: {
+        urlFilter: "||ecoledirecte.com",
+        resourceTypes: ["xmlhttprequest", "main_frame", "sub_frame"],
+        excludedInitiatorDomains: ["www.ecoledirecte.com", "ecoledirecte.com"]
+      },
+      action: {
+        type: "modifyHeaders",
+        requestHeaders: [
+          {
+            header: "X-GTK",
+            operation: "set",
+            value: gtk
+          },
+          {
+            header: "Cookie",
+            operation: "set",
+            value: `GTK=${gtk}`
+          },
+        ]
+      },
+    });
+  }
+
+  await chrome.declarativeNetRequest.updateDynamicRules({
+    removeRuleIds: removeRuleIds,
+    addRules: rules
+  });
+
+  chrome.tabs.query({url: "*://*.ecole-directe.plus/*"}, tabs => {
+    tabs.forEach(tab => {
+      chrome.tabs.sendMessage(tab.id, {
+        action: "gtkRulesUpdated"
+      });
+    });
+  });
+}
+
+function interceptCookieGTK(details) {
+  let headers = details.responseHeaders;
+
+  for (const { name, value } of headers) {
+    if (name !== "set-cookie") continue;
+    if (!value.startsWith("GTK")) return;
+
+    const parts = value.split(";")[0];
+    const cookie = parts.split("=")[1];
+    if (cookie) {
+      gtk = cookie;
+      updateGtkRules();
+    }
+  }
+
+  return { responseHeaders: headers };
+}
+
+chrome.webRequest.onHeadersReceived.addListener(
+  interceptCookieGTK,
+  { urls: ["*://api.ecoledirecte.com/*"] },
+  ["responseHeaders", "extraHeaders"]
+);
