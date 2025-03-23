747cd53504f78e1c17fa1fc0583836f7bb64a32b
Truite Séchée <truite.berthail@gmail.com>
2025-03-23 15:37:58 +0100

    now only updates the reset password page if the referrer is EDP, some refactoring

--- content-scripts/resetPassword.js
+++ content-scripts/resetPassword.js
@@ -1,4 +1,5 @@
-(() => {
+const injectEDPStyles = () => {
+	document.documentElement.classList.add("edp");
 	const img = document.querySelector("#A7");
 	const isWhite = (value) => value > 50 && value < 256;
 	const isBlack = (value) => value > 0 && value < 50;
@@ -51,4 +52,8 @@
 	}, "image/png");
 	document.querySelector("button#A8").innerHTML =
 		'<svg width="20" height="20" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill="#fff" d="M5.516 14.224c-2.262-2.432-2.222-6.244.128-8.611a6.07 6.07 0 0 1 3.414-1.736L8.989 1.8a8.1 8.1 0 0 0-4.797 2.351c-3.149 3.17-3.187 8.289-.123 11.531l-1.741 1.752 5.51.301-.015-5.834zm6.647-11.959.015 5.834 2.307-2.322c2.262 2.434 2.222 6.246-.128 8.611a6.07 6.07 0 0 1-3.414 1.736l.069 2.076a8.12 8.12 0 0 0 4.798-2.35c3.148-3.172 3.186-8.291.122-11.531l1.741-1.754z"/></svg>';
-})();
+}
+
+if (new URL(document.referrer).hostname.includes("ecole-directe.plus")) {
+	injectEDPStyles();
+}
